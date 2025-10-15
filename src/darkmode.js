5950fc1e05591109ec18b85815c50dc0d11d4812
domanskey <domadam04@gmail.com>
2025-10-15 13:22:35 +0200

    add darkmode function

--- src/darkmode.js
+++ src/darkmode.js
@@ -1,5 +1,22 @@
-const darkmode = document.querySelector('.theme-toggle');
 
-darkmode.addEventListener('click', (e) => {
-    document.body.classList.toggle('darkmode');
+let darkmode = localStorage.getItem('darkmode');
+const themeToggle = document.querySelector('.theme-toggle');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'enabled');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', 'disabled');
+}
+
+if (darkmode === 'enabled') {
+    enableDarkmode();
+}
+
+themeToggle.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== 'enabled' ? enableDarkmode() : disableDarkmode();
 });
